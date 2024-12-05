5e95d2e37951e404bf8e148945f5a489f8a73b3e
MTNV2024 <mtnv@demant.com>
2024-12-05 13:35:11 +0100

    Update configModal.js

--- configModal.js
+++ configModal.js
@@ -2,7 +2,7 @@ document.getElementById('configForm').addEventListener('submit', function(event)
   event.preventDefault();
 
   // Set the default GitHub Pages URL here
-  const defaultWebhookURL = 'https://mtnv2024.github.io/TeamsIntegrationDemandgen/webhookEndpoint'; // <-- Your GitHub Pages URL
+  const defaultWebhookURL = 'https://demant.webhook.office.com/webhookb2/9ef097e3-8845-441a-ba27-b99a1d0a0df4@9bf8c7a8-e008-49a7-9e43-ab76976c4bf8/IncomingWebhook/28311e0803c14770b2ecb4a1173bf096/91002fde-077d-4dec-95a4-b3867a2c7509/V2kA45SCs54X3bWcWs_4KJzGryKb4q56AocpVu5YALryE1'; // <-- Your GitHub Pages URL
   const webhookURL = document.getElementById('webhookURL').value || defaultWebhookURL; // Use the default if no input
   const message = document.getElementById('message').value;
 
